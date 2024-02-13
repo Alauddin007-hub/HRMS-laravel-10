de9e97f787b7ebbe68afcfe8c9e3e998ecb8bf0b
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2024-02-13 08:19:07 +0600

    crud

---
+++ database/migrations/2024_02_12_155856_create_advances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advances', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('employee_id');
+            $table->string('month_of_advance');
+            $table->string('apply_amount');
+            $table->string('approved_amount')->nullable();
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advances');
+    }
+};
